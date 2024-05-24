f09b7eb851a3a486a1d9e80c14a0bcd4f0544f07
macluiggy <luiggy.macias@imagineapps.co>
2024-05-23 23:29:39 -0500

    refactor: Make username and password fields optional in UserSerializer
    This commit modifies the UserSerializer in the serializers.py file to make the username and password fields optional when updating an existing user. By setting these fields as non-required, it improves the flexibility of the serializer and allows for more seamless user updates.

--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -3,17 +3,13 @@ from django.contrib.auth.models import User
 from posts.serializers import PostSerializer
 
 class UserSerializer(serializers.ModelSerializer):
-    # posts = PostSerializer(many=True, read_only=True)
-    posts = serializers.SerializerMethodField()
+    posts = PostSerializer(many=True, read_only=True)
     class Meta:
         model = User
         fields = '__all__'
-
+    # make non-required fields for updating user
     def __init__(self, *args, **kwargs):
         super(UserSerializer, self).__init__(*args, **kwargs)
         if self.instance is not None:  # Updating an existing user
             self.fields['username'].required = False
             self.fields['password'].required = False
-            
-    def get_posts(self, obj):
-        return PostSerializer(obj.post_set.all(), many=True).data
