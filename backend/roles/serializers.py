8dcc1ce842906921414b50a96d1413d1171b25e0
macluiggy <luiggy.macias@imagineapps.co>
2024-05-30 11:47:54 -0500

    feat: Add 'country' field to 'RolesSerializer'
    This commit adds the 'country' field to the 'RolesSerializer' in the 'roles/serializers.py' file. The 'country' field is serialized using the 'CountrySerializer'. This change enhances the functionality of the application by including the country information in the serialized output of the 'Role' model.

--- backend/roles/serializers.py
+++ backend/roles/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from country.models import Country
 from .models import Role, UserRole
 from area.serializers import AreaSerializer
+from country.serializers import CountrySerializer
 class RolesSerializer(serializers.ModelSerializer):
+    country = CountrySerializer()
     class Meta:
         model = Role
         fields = '__all__'
