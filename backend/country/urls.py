5dedb742b16e8de1a80c1b02e6dcc8fc06a69f25
macluiggy <luiggy.macias@imagineapps.co>
2024-05-30 10:22:11 -0500

    feat: Add 'area', 'country', 'permissions', and 'roles' URLs
    This commit adds the URLs for the 'area', 'country', 'permissions', and 'roles' models in the backend. These URLs provide endpoints for managing areas, countries, permissions, and roles. This change enhances the functionality of the application by allowing users to perform CRUD operations on these entities.

---
+++ backend/country/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    # path('add/<int:post_id>', views.add_comment, name='add_like'),
+    # path('remove/<int:post_id>', views.remove_comment, name='remove_like'),
+]
